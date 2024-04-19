1dd8529f458fa8233e1be3c52fd38a91cc6126dc
VitorCarvalho67 <vitor.carvalho67@etec.sp.gov.br>
2024-04-18 21:45:52 -0300

    Update CreateAlunoUseCase use case to handle different scenarios

--- server/src/modules/alunos/useCase/createAluno/CreateAlunoUseCase.ts
+++ server/src/modules/alunos/useCase/createAluno/CreateAlunoUseCase.ts
@@ -2,43 +2,65 @@ import { Aluno } from "@prisma/client";
 import { prisma } from "../../../../prisma/client";
 import { CreateAlunoDTO } from "../../dtos/CreateAlunoDTO";
 import { AppError } from "../../../../errors/error";
-
-const bcrypt = require('bcrypt');
+import e from "express";
 
 export class CreateAlunoUseCase {
     async execute({email, token} : CreateAlunoDTO): Promise<Aluno>{
 
         const preAluno = await prisma.preAluno.findUnique({
             where: {
-                email,
-                token
-            }
-        });
-
-        if (!preAluno){
-            throw new AppError("PreAluno não existe");
-        }
-
-        const aluno = await prisma.aluno.create({
-            data:{
-                name: preAluno.name,
-                email,
-                password: preAluno.password
-            }
-        });
-
-        // Deletar preAluno
-        await prisma.preAluno.delete({
-            where: {
                 email
             }
         });
 
-        // ir decrementando a quantidade de tentativas validas caso ele encontre o email
-        // mas com o token errado
+        if (preAluno){
+            const preAluno = await prisma.preAluno.findUnique({
+                where: {
+                    email,
+                    token
+                }
+            });
 
-        // deletar caso as tentativas sejam 0 e caso o tempo de validade tenha passado(10min)
-        
-        return aluno;
+            if (preAluno){
+                if(preAluno.tentativasRestantes <= 0 || (preAluno.createdAt.setMinutes(preAluno.createdAt.getMinutes() + 10)) > Date.now()) {
+                    await prisma.preAluno.delete({
+                        where: {
+                            email
+                        }
+                    });
+                    throw new AppError("Token expirado");    
+                }else {
+                    const aluno = await prisma.aluno.create({
+                        data:{
+                            name: preAluno.name,
+                            email,
+                            password: preAluno.password
+                        }
+                    });
+            
+                    await prisma.preAluno.delete({
+                        where: {
+                            email
+                        }
+                    });
+                    return aluno;
+                }
+
+            }else {
+                await prisma.preAluno.update({
+                    where: {
+                        email
+                    },
+                    data: {
+                        tentativasRestantes: {
+                            decrement: 1
+                        }
+                    }
+                });
+                throw new AppError("Token inválido");
+            }
+        }else {
+            throw new AppError("Email não encontrado");
+        }
     }
 }
