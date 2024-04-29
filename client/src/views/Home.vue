a5824aa3ea7a7dbcabdf13d87177ac33bf020ebc
VitorCarvalho67 <vitor.carvalho67@etec.sp.gov.br>
2024-04-29 18:20:34 -0300

    Updade on Home view

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -3,6 +3,26 @@
     <div id="app">
         <main>
             Hello World!
+            <br>
+            <router-link to="/login">Login Aluno</router-link>
+            <br>
+            <router-link to="/admin/login">Login Admin</router-link>
+            <br>
+            <router-link to="/register">Register Aluno</router-link>
+            <br>
+            <router-link to="/register/token">Confirmação do token</router-link>
+            <br>
+            <router-link to="/recovery">Recuperar senha</router-link>
+            <br>
+            <router-link to="/recovery/validate">Token de recuperação de senha</router-link>
+            <br>
+            <router-link to="/admin/register/professor">Registro de professores pelo admin</router-link>
+            <br>
+            <router-link to="/admin/register/coordenador">Registro de Coordenador pelo admin</router-link>
+            <br>
+            <router-link to="/admin/register/curso">Registro de Curso pelo admin</router-link>
+            <br>
+
         </main>
     </div>
     <Footer />
