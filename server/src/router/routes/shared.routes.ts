4ea5ddeee6e442529d563a797acffbb6b8c211b9
Daniel-Alvarenga <daniel07alvarenga2023@gmail.com>
2024-08-09 16:35:47 -0300

    Add shared get extracurriculares route

--- server/src/router/routes/shared.routes.ts
+++ server/src/router/routes/shared.routes.ts
@@ -10,5 +10,6 @@ sharedRoutes.get("/profile/banner", controllers.getBannerController.handle);
 sharedRoutes.get("/users", controllers.searchUserController.handle);
 sharedRoutes.get("/vagas", controllers.getVagasController.handle);
 sharedRoutes.get("/vaga", controllers.getVagaController.handle);
+sharedRoutes.get("/extracurriculares", controllers.getExtracurricularsByRMController.handle);
 
 export { sharedRoutes };
