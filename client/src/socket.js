77af0cb76ebe3c49f949325f1c878a66e07eec5a
Daniel-Alvarenga <daniel07alvarenga2023@gmail.com>
2024-08-21 21:40:20 -0300

    Add env file to client

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 import { reactive } from 'vue';
 
-export const server_url = "http://localhost:3333";
+export const server_url = import.meta.env.SERVER_URL;
 // export const server_url = "https://f02f7c405f4e1e6fce00528eb2719719.serveo.net/";
 
 export const socket = io(server_url);
